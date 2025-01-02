6051060eabdad40c77944132785f1b330b82ce20
Jay Patel <japatel017@gmail.com>
2025-01-02 22:02:17 +0530

    ok

--- controllers/url.js
+++ controllers/url.js
@@ -12,6 +12,7 @@ async function handleGenerateNewShortUrl(req, res) {
     shortId: shortId,
     redirectURL: body.url,
     visitHistory: [],
+    createdBy: req.user._id,
   });
 
   return res.render("home", {
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const path = require("path");
 const { connectToMongoDB } = require("./connect");
+const cookieParser = require("cookie-parser");
+const URL = require("./models/url");
+const {restrictToLoggedInUsersOnly,checkAuth}=require('./middlewares/auth')
+
 const staticRoute = require("./routes/staticRouter");
 const urlRoute = require("./routes/url");
-const URL = require("./models/url");
+const userRoute=require('./routes/user');
+
 const app = express();
 const PORT = 8001;
 
@@ -16,6 +21,7 @@ app.set("views", path.resolve("./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get("/test", async (req, res) => {
   const allUrls = await URL.find({});
@@ -24,7 +30,8 @@ app.get("/test", async (req, res) => {
   });
 });
 
-app.use("/url", urlRoute);
+app.use("/url", restrictToLoggedInUsersOnly,urlRoute);
+app.use('/user',checkAuth,userRoute);
 app.use("/", staticRoute);
 
 app.get("/url/:shortId", async (req, res) => {
